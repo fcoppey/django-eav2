10aa1374e55819dc90e5843bd0b3507db21e64a0
Florentin <florentin@nirlab.ch>
2021-08-22 23:12:59 +0200

    another attempt to fix

--- eav/helpers.py
+++ eav/helpers.py
@@ -1,11 +1,4 @@
 from eav.models import EnumValue
 
 def unspecified():
-    try:
-        return EnumValue.objects.get(value='unspecified')
-    except:
-        return EnumValue.objects.create(
-            value='unspecified',
-            value_display='Unspecified',
-            value_display_fr='Non spécifié'
-        )
+    return EnumValue.objects.get_or_create(value='unspecified')[0]
