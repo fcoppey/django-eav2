e2b121f30dcd922d01e23b6212f7c7df616e0df6
malikbeytrison <malik.beytrison@apptitude.ch>
2022-02-15 12:28:50 +0100

    remove ugettext_lazy

--- eav/models.py
+++ eav/models.py
@@ -17,7 +17,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.base import ModelBase
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from sortedm2m.fields import SortedManyToManyField
 
